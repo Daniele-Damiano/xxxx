9e87e28ec29a4103116f6377bc61a8a54910731f
DamianoDaniele <damianodaniele19@gmail.com>
2017-07-11 10:53:24 +0200

    sdfsdf

---
+++ sdsdsd/src/main/java/sdsdsd/Messaggio.java
@@ -0,0 +1,19 @@
+package sdsdsd;
+
+public class Messaggio {
+	private String mes;
+
+	public Messaggio(String mes) {
+		super();
+		this.mes = mes;
+	}
+
+	public String getMes() {
+		return mes;
+	}
+
+	public void setMes(String mes) {
+		this.mes = mes;
+	}
+		
+}
--- sdsdsd/src/main/java/sdsdsd/Starter.java
+++ sdsdsd/src/main/java/sdsdsd/Starter.java
@@ -2,6 +2,14 @@ package sdsdsd;
 
 public class Starter {
 	public static void main(String[] args) {
+		if(args.length < 0)
+			System.out.println("Devi inserire un argomento");
 		System.out.println("ddx");
+		Messaggio c = new Messaggio("ciao");
+		System.out.println(c.getMes());
+		
+		String provaArgs = args[0];
+		Messaggio cd = new Messaggio(provaArgs);
+		System.out.println(cd.getMes());
 	}
 }
